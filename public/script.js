a9edc1f0f5ab82e51840dc11ce38f34810a1799c
Nour Boulahcen <nour.boulahcen@gmail.com>
2021-03-14 01:39:45 +0100

    finish implementing chat rooms

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+if (messageForm != null) {
+    const name = prompt('What is your name?')
+    appendMessage('You joined')
+    socket.emit('new-user', roomName, name)
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value
+        appendMessage(`You : ${message}`)
+        socket.emit('send-chat-message', roomName, message)
+        messageInput.value = ''
+    })
+}
+
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name} : ${data.message}`)
@@ -18,12 +30,14 @@ socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`)
 })
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You : ${message}`)
-    socket.emit('send-chat-message', message)
-    messageInput.value = ''
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = 'join'
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
 })
 
 function appendMessage(message) {
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const server = require('http').Server(app)
 const io = require("socket.io")(server)
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
@@ -14,25 +22,50 @@ app.get('/', (req, res) => {
     res.render('index', { rooms: rooms })
 })
 
-app.get('/:room', (res, req) => {
-    res.render('room', { roomName: req.params.room })
+app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+        return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} }
+    res.redirect(req.body.room)
+    io.emit('room-created', req.body.room)
 })
+
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+        return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room })
+
+})
+
+
 
 server.listen(3000)
 
 const users = {}
 
 io.on('connection', socket => {
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+    socket.on('new-user', (room, name) => {
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).broadcast.emit('user-connected', name)
     })
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    socket.on('send-chat-message', (room, message) => {
+        socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
     })
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
+        getUserRooms(socket).forEach(room => {
+            socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id]
+        })
     })
 })
+
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if (room.users[socket.id] != null) names.push(name)
+        return names
+    }, [])
+}
